50efc115ebdd242727659169150d2e1378549854
Takeaio <iorizhunei2@gmail.com>
2024-11-15 18:06:58 +0900

    入力スペースの作成

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Input from "../components/input"
+
+export default function Home() {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
---
+++ frontend/components/input.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from 'react'
+
+
+const styles = {
+    box:`
+
+    `,
+    InputBox:`
+        text-black
+        text-2xl
+        lg:w-96
+        lg:h-24
+        h-24
+        w-48
+        rounded 
+        overflow-hdden
+        shadow-lg
+        relative
+        border border-gray-500
+        text-center
+    `
+}
+
+
+
+const input = () => {
+  return (
+    <div className={styles.box} >
+        <input className={styles.InputBox} type='text' /*value={userInput} onChange={handleInputChange}*/ placeholder='ここにタイピングしてくれ' />
+        
+    </div>
+  )
+}
+
+export default input
