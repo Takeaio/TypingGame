964741cb1aa1ac772ced3bae2497d0274fb37ddb
Takeaio <iorizhunei2@gmail.com>
2024-11-20 17:41:33 +0900

    restartボタンの編集

--- frontend/components/ResetButton.tsx
+++ frontend/components/ResetButton.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
+const styles={
+    container:`
+        flex 
+        items-center 
+        justify-center
+    `,
+    button: `
+        bg-blue-500
+        text-white
+        py-2
+        px-4
+        rounded
+        shadow-lg
+        hover:bg-blue-700
+        focus:outline-none
+        focus:ring-2
+        focus:ring-blue-500
+    `
+}
+
+
 interface ResetButtonProps {
   onClick: () => void;
 }
 
 const ResetButton: React.FC<ResetButtonProps> = ({ onClick }) => {
-  return <input id="reset" type="button" value="Reset" onClick={onClick} />;
+  return (
+    <div className={styles.container}>
+        <input className={styles.button} id="reset" type="button" value="Restart" onClick={onClick} />
+    </div>
+
+
+  )
 };
 
 export default ResetButton;
