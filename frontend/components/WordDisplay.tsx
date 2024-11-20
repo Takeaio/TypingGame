c0ff1853ca8e48ec41ca5eda15500a084629664c
Takeaio <iorizhunei2@gmail.com>
2024-11-20 17:57:52 +0900

    worddisplayの編集

--- frontend/components/WordDisplay.tsx
+++ frontend/components/WordDisplay.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 
+const styles={
+    container:`
+        flex
+        items-center
+        justify-center
+    `,
+    title:`
+        text-4xl
+    `,
+    word:`
+        text-center
+        text-8xl
+        
+    `
+}
+
+
 interface WordDisplayProps {
-  word: string;
+    word: string;
 }
 
 const WordDisplay: React.FC<WordDisplayProps> = ({ word }) => {
-  return <h2>Word: {word}</h2>;
+    return (
+        <div className={styles.container}>
+            <div className="flexflex-row">
+                <h2 className={styles.title}>
+                    次の文字をタイピングするのだ！！
+                </h2>
+                <h2 className={styles.word}>
+                    {word}
+                </h2>
+            </div>
+        </div>
+
+    )
 };    
 
 export default WordDisplay;
